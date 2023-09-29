6f14f04a0e95131f40637f8d7c237f4edba50eed
FelipeFabricio <ff.oliveira32@gmail.com>
2023-09-29 01:04:36 -0300

    Implement product_db and its interface

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -1,8 +1,18 @@
 package database
 
-import "github.com/felipefabricio/golang-rest-api/internal/entity"
+import (
+	"github.com/felipefabricio/golang-rest-api/internal/entity"
+)
 
 type UserInterface interface {
 	Create(client *entity.Client) error
 	FindByEmail(email string) (*entity.Client, error)
+}
+
+type ProductInterface interface {
+	Create(product *entity.Product) error
+	ListAllProducts(page, limit int, sort string) ([]*entity.Product, error)
+	FindById(id string) *entity.Product
+	Update(product *entity.Product) error
+	Delete(id string) error
 }
